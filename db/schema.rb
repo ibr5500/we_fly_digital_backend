484cb95344bd9d16c91c7a88121f937b189741ed
NatiG <nati.g5684@gmail.com>
2022-09-24 00:09:07 +0300

    Create a new user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_23_152840) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_23_210553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_152840) do
     t.string "name"
     t.string "surname"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
