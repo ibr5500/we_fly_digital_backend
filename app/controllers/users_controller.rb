e910b0cbc503d9cd6289c739a1427ab4e7061901
NatiG <nati.g5684@gmail.com>
2022-09-24 00:45:37 +0300

    Add a login action method for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   def create
-    @user = User.create(user_params)
+    if User.find_by_email(user_params[:email])
+      render json: { error: 'Email exits, try a diffrent one' }, status: :not_acceptable
+    else 
+      @user = User.create(user_params)
 
     if @user.valid?
       token = encode_token({ user_id: @user.id })
@@ -9,30 +12,22 @@ class UsersController < ApplicationController
       render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
     end
   end
+end
+
+def login
+  @user = User.find_by(email: user_params[:email])
+
+  if @user && @user.authenticate(user_params[:password])
+    token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: token }, status: :ok
+    else
+      render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
+    end
+end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :surname, :email, :password_digest)
+    params.require(:user).permit(:name, :surname, :email, :password)
   end
 end
-
-
-
-#     if User.find_by_email(user_params[:email])
-#       render json: {
-#         error: 'Email exits, try a diffrent one' 
-#     }, 
-#       status: :not_acceptable
-#   else 
-#     user = User.create(user_params)
-#     if user.save
-#       token = issue_token(user)
-#     render json: {
-#       error: 'User created successfully!' 
-#   }, 
-#     status: :acceptable
-#   end
-
-
-# end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, only: [:create]
     end
   end
+  post "/login", to: "users#login"
 end
