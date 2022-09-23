1af3117fbca5b5d1c7a7538473553c091fdf93e3
NatiG <nati.g5684@gmail.com>
2022-09-23 22:46:37 +0300

    Create an action to register a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,38 @@
 class UsersController < ApplicationController
   def create
-    if User.find_by_email(user_params[:email])
-      render json: {
-        error: 'Email exits, try a diffrent one' 
-    }, 
-      status: :not_acceptable
-  else 
-    user = User.create(user_params)
-    if user.save
-      token = issue_token(user)
-    render json: {
-      error: 'User created successfully!' 
-  }, 
-    status: :acceptable
+    @user = User.create(user_params)
+
+    if user.valid?
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: token }, status: :ok
+    else
+      render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
+    end
   end
 
   private
 
   def user_params
-    params.permit(:name, :surname, :email, :password)
+    params.require(:users).permit(:name, :surname, :email, :password)
   end
 end
+
+
+
+#     if User.find_by_email(user_params[:email])
+#       render json: {
+#         error: 'Email exits, try a diffrent one' 
+#     }, 
+#       status: :not_acceptable
+#   else 
+#     user = User.create(user_params)
+#     if user.save
+#       token = issue_token(user)
+#     render json: {
+#       error: 'User created successfully!' 
+#   }, 
+#     status: :acceptable
+#   end
+
+
+# end
