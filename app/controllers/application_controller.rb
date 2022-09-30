337efb3d9f1d2d196e34cfb11542e1dbce88e590
NatiG <nati.g5684@gmail.com>
2022-09-30 17:56:11 +0300

    Add authorize methode to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::API
   def encode_token(payload)
     JWT.encode(payload, 'secret')
   end
+
+  def decode_token
+    # Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMTIifQ.AYoDvVWjqV8pcJTqtd4ypw5h9FeFjnFvB3mStsn-LpY 
+
+    auth_header = request.headers('Authorization')
+
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'secret', true, algorithm: 'HS256')
+        rescue JWT::DecodeError
+          nil
+        end
+    end
+  end
+
+  def authorize_user
+    decode_token = decode_token()
+    if decode_token
+      user_id = decode_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
 end
