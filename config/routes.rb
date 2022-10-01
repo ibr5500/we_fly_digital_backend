b0ca068db34a4cd50ade8977e46b450129f88c0c
NatiG <nati.g5684@gmail.com>
2022-10-01 16:39:02 +0300

    Remove all api /v1routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,7 @@ Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:create, :index]
   resources :reservations, only: [:create, :index]
-
-  namespace :api do
-    namespace :v1 do
-      resources :users, only: [:create, :index]
-      resources :reservations, only: [:create, :index]
-      resources :airlines, only: [:index, :show]
-    end
-  end
+  resources :airlines, only: [:index, :show, :create, :destroy]
 
   post "/login", to: "users#login"
 end
