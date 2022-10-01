e5527622cdf02aa93ba4f526085785224c68efc6
GOlivierNation <ogasominali@gmail.com>
2022-10-01 08:49:40 -0400

    Add Airline validation

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,6 @@
 class Airline < ApplicationRecord
   has_many :reservations, dependent: :destroy
+
+  validates :name, :image, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
