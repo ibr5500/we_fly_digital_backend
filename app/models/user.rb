2463831ce61d9362c0e1805513ffddd186c0848c
NatiG <nati.g5684@gmail.com>
2022-10-01 16:38:38 +0300

    Add association from airlines to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
+  has_many :airlines, foreign_key: 'airline_id', dependent: :destroy
 
   validates :fullname, :username, :email, :password, presence: true
 end
