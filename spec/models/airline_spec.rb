1920f0ef97c7c8a893203965cff6c027b3ca0ca6
GOlivierNation <ogasominali@gmail.com>
2022-10-05 16:33:28 -0400

    Add test for Airline Model

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Airline, type: :model do
   subject do
     airline = Airline.new(
-      name: 'Rwandair'
+      name: 'Rwandair',
+      price: '100',
+      user_id: '1'
     )
 
     return airline
@@ -13,6 +15,11 @@ RSpec.describe Airline, type: :model do
     it 'An Airline should have a valid name' do
       subject.name = 'amsterdam'
       expect(subject).to_not be_valid
+    end
+    
+    it 'An Airline should have a valid user_id' do
+        subject.user_id = '2'
+        expect(subject).to_not be_valid
     end
   end
 end
