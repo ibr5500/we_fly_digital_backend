60a269a4568260ad07bfc5a6e24b837b2e4b7977
unknown <ibr.s.ahmat@gmail.com>
2022-10-06 19:37:15 +0300

    Add validatetion for uniquenesses

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -2,6 +2,7 @@ class Airline < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
   has_many :reservations, dependent: :destroy
 
-  validates :name, :image, presence: true
+  validates :name, uniqueness: true, presence: true
+  validates :image, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :airlines, foreign_key: 'airline_id', dependent: :destroy
 
-  validates :fullname, :username, :email, :password, presence: true
+  validates :password, presence: true
+  validates :fullname, :email, :username, uniqueness: true, presence: true 
 end
